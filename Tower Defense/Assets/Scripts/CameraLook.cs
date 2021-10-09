fa374bc4cdcec94e99869ae3a59a6a808cea6b2e
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2021-10-08 22:34:09 -0500

    Added Movement and Animations

---
+++ Tower Defense/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public GameObject playerMesh;
+
+    public float mouseSensibility;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensibility * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensibility * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerMesh.transform.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Tower Defense/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * vert + transform.right * horz;
+
+        transform.position += move * speed * Time.deltaTime;
+
+    }
+}
