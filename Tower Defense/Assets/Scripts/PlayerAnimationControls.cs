fa3038f355e61457d18d1523e46b53a8e131dd00
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2021-10-09 17:24:48 -0500

    Finished Player 2D Animation Tree

--- Tower Defense/Assets/Scripts/PlayerAnimationControls.cs
+++ Tower Defense/Assets/Scripts/PlayerAnimationControls.cs
@@ -7,55 +7,128 @@ public class PlayerAnimationControls : MonoBehaviour
     public Animator animator;
     public GameObject camera;
 
-    public Vector3 camOffset;
+    public Vector3 forwardOffset;
+    public Vector3 leftOffset;
+    public Vector3 rightOffset;
+    public Vector3 backOffset;
+
     public int animationTime;
     public float camStatus;
 
     Vector3 camDefault;
 
-    float animationStatus = 0;
+    Vector3 animationStatus = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         camDefault = new Vector3(camera.transform.localPosition.x, camera.transform.localPosition.y, camera.transform.localPosition.z);
-        camOffset += camDefault;
+        forwardOffset += camDefault;
+        leftOffset += camDefault;
+        rightOffset += camDefault;
+        backOffset += camDefault;
     }
 
     // Update is called once per frame
     void Update()
     {
         float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
 
-        if (vert == 1)
+        if (Input.GetKey("w"))
         {
-            if (animationStatus < animationTime)
+            if (animationStatus.z < animationTime)
             {
-                animationStatus += Time.deltaTime;
-            } else if (animationStatus > animationTime)
-            {
-                animationStatus = animationTime;
-            }
+                animationStatus.z += Time.deltaTime;
+            } 
 
-            animator.SetFloat("Blend", animationStatus);
-        } else
+            animator.SetFloat("Velocity Z", animationStatus.z);
+        } 
+        else
         {
-            if (animationStatus > 0)
+            if (animationStatus.z > 0)
             {
-                animationStatus -= 2.5f * Time.deltaTime;
+                animationStatus.z -= 2.5f * Time.deltaTime;
             }
-            else if (animationStatus < 0)
-            {
-                animationStatus = 0;
-            }
+            else 
 
-            animator.SetFloat("Blend", animationStatus);
+            animator.SetFloat("Velocity Z", animationStatus.z);
         }
 
-        if (animationStatus > camStatus)
+        if (Input.GetKey("d"))
         {
-            camera.transform.localPosition = camOffset;
-        } else
+            if (animationStatus.x < animationTime)
+            {
+                animationStatus.x += Time.deltaTime;
+            } 
+
+            animator.SetFloat("Velocity X", animationStatus.x);
+        } 
+        else
+        {
+            if (animationStatus.x > 0)
+            {
+                animationStatus.x -= 2.5f * Time.deltaTime;
+            } 
+
+            animator.SetFloat("Velocity X", animationStatus.x);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            if (animationStatus.z > -animationTime)
+            {
+                animationStatus.z -= Time.deltaTime;
+            }
+
+            animator.SetFloat("Velocity Z", animationStatus.z);
+        }
+        else
+        {
+            if (animationStatus.z < 0)
+            {
+                animationStatus.z += 2.5f * Time.deltaTime;
+            }
+
+            animator.SetFloat("Velocity Z", animationStatus.z);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            if (animationStatus.x > -animationTime)
+            {
+                animationStatus.x -= Time.deltaTime;
+            }
+
+            animator.SetFloat("Velocity X", animationStatus.x);
+        }
+        else
+        {
+            if (animationStatus.x < 0)
+            {
+                animationStatus.x += 2.5f * Time.deltaTime;
+            }
+
+            animator.SetFloat("Velocity X", animationStatus.x);
+        }
+
+        if (animationStatus.z > camStatus)
+        {
+            camera.transform.localPosition = forwardOffset;
+        } 
+        else if (animationStatus.z < -camStatus)
+        {
+            camera.transform.localPosition = backOffset;
+        } 
+        else if (animationStatus.x > camStatus)
+        {
+            camera.transform.localPosition = rightOffset;
+        } 
+        else if (animationStatus.x < -camStatus)
+        {
+            camera.transform.localPosition = leftOffset;
+        }
+        else
         {
             camera.transform.localPosition = camDefault;
         }
