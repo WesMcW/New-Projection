deedf06367a9b13de0489d7cbfe210b41d50d208
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2021-10-09 21:40:40 -0500

    Deactivated Ragdoll

--- Tower Defense/Assets/Scripts/EnemyAnimationControls.cs
+++ Tower Defense/Assets/Scripts/EnemyAnimationControls.cs
@@ -5,54 +5,23 @@ using UnityEngine.AI;
 
 public class EnemyAnimationControls : MonoBehaviour
 {
-    public float deathTime;
-
     Animator animator;
-
-    float waitingTime;
-    float velocity;
-
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-
-        waitingTime = 0;
-        velocity = 0;
-
-        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
-        {
-            rb.isKinematic = true;
-
-            Collider col = rb.GetComponent<Collider>();
-            col.isTrigger = true;
-        }
-
-        animator.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (waitingTime <= deathTime && waitingTime < deathTime + 1)
-        {
-            waitingTime += Time.deltaTime;
-        }
-
-        if (waitingTime > deathTime)
-        {
-            gameObject.GetComponentInParent<NavMeshAgent>().enabled = false;
-            enableRagdoll();
-        }
-
-        animator.SetFloat("Velocity", velocity);
 
     }
 
     void enableRagdoll()
     {
+        gameObject.GetComponentInParent<NavMeshAgent>().enabled = false;
         foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
         {
             rb.isKinematic = false;
